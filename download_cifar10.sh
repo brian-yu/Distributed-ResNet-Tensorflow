29e3597daeed7af2fe0b75268f9b7f2ad710bc3a
Brian Yu <brian@byu.io>
2019-11-18 10:26:01 -0500

    Download stuff scripts

---
+++ download_cifar10.sh
@@ -0,0 +1,3 @@
+mkdir data/
+python3 cifar10_download_and_extract.py
+mv /tmp/cifar10_data/cifar-10-batches-bin ./data/
--- download_cifar100.sh
+++ download_cifar100.sh
@@ -1,3 +1,3 @@
 wget https://www.cs.toronto.edu/~kriz/cifar-100-binary.tar.gz
-
+mkdir data/
 tar -xf cifar-100-binary.tar.gz -C data/
